f3704c53fc5735a3546d4bdb6523f7451e90932c
Carmelo Alejandro García Almeida <carmelo.garcia@udc.es>
2025-05-31 14:38:42 +0200

    Update ejemplo2.py

--- ejemplo2/ejemplo2.py
+++ ejemplo2/ejemplo2.py
@@ -1,9 +1,20 @@
 #/usr/bin/python3
 
+class Ranking:
+  def __init__(self):
+    self.data = {}
 
-##############################################
-#### Pon las definiciones que faltan aquí ####
-##############################################
+  def __setitem__(self,key,value):
+    self.data[key] = value
+
+  def __getitem__(self, key):
+    return self.data[key]
+
+  def __str__(self):
+    result = ""
+      for key in sorted(self.data):
+        result += f"{key}: {self.data[key]}\n"
+      return result.strip()  
 
 r=Ranking()
 r[1]="Amancio Ortega"
