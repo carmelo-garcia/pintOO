b76234e5de9d0f92191872b6b5bc7ec0a42d6527
Carmelo Alejandro García Almeida <carmelo.garcia@udc.es>
2025-05-31 14:42:46 +0200

    Update ejemplo3.py

--- ejemplo3/ejemplo3.py
+++ ejemplo3/ejemplo3.py
@@ -1,9 +1,31 @@
 #/usr/bin/python3
 
 
-##############################################
-#### Pon las definiciones que faltan aquí ####
-##############################################
+class Movimiento:
+  saldo = 0
+
+
+  def __init__(self, cantidad):
+    self.cantidad = cantidad
+    self.aplicar()
+
+  def aplicar(self):
+    pass  # Será implementado en las subclases
+
+  @classmethod
+  def getSaldo(cls):
+    return cls.saldo
+
+
+class Ingreso(Movimiento):
+    def aplicar(self):
+        Movimiento.saldo += self.cantidad
+
+
+class Gasto(Movimiento):
+    def aplicar(self):
+        Movimiento.saldo -= self.cantidad
+
 
 ingreso1=Ingreso(1000)
 gasto1=Gasto(100)
