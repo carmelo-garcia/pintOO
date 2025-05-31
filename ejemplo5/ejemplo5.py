9083248688a30c2030d293d421b4f186daad904d
Carmelo Alejandro García Almeida <carmelo.garcia@udc.es>
2025-05-31 14:47:21 +0200

    Update ejemplo5.py

--- ejemplo5/ejemplo5.py
+++ ejemplo5/ejemplo5.py
@@ -4,5 +4,21 @@
 from symbols.Variable import Variable
 from values.Number import Number
 
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Variable:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def __str__(self):
+        return f"Variable {self.name} has value {self.value}"
+      
 v=Variable("a",Number(3))
 print(v)
